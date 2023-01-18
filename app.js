3ec5b57df83cb6b5644c9ce7bf330f2c2e38f258
sanket Bhalerao <sbrao666@gmail.com>
2023-01-18 14:36:23 +0530

    Settting up the nodejs server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+const express = require("express");
+
+
+const app = express()
+
+app.use(express.json()) //It parses incoming requests with JSON payloads.
+const port = process.env.PORT
+
+app.listen(port, async () => {
+    console.log("Server is up and running on port : " + port)
+})
